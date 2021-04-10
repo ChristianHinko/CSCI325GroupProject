67063d8dff344ac2f5298b046407ee4775a3830d
include-iostream <c4hinkle@gmail.com>
2021-04-10 18:00:54 -0300

    Add labels to algebra graph and add digit counter in advanced calculator globals and clean up tolerance thing

--- AdvancedCalculator3000/src/advancedcalculator3000/Calculator/AdvancedCalculatorGlobals.java
+++ AdvancedCalculator3000/src/advancedcalculator3000/Calculator/AdvancedCalculatorGlobals.java
@@ -26,11 +26,7 @@ public class AdvancedCalculatorGlobals
     {
         double tolerance = KINDA_SMALL_NUMBER;
         
-        if (Math.abs(a - b) <= tolerance)
-        {
-            return true;
-        }
-        return false;
+        return IsNearlyEqual(a, b, tolerance);
     }
     /**
      * Returns true if 2 doubles are nearly equal with a specified tolerance
@@ -66,6 +62,40 @@ public class AdvancedCalculatorGlobals
         if (number % 1 == 0)
         {
             retVal = "" + (int)number;
+        }
+        
+        return retVal;
+    }
+    
+    /**
+     * Does not account for negative signs by default
+     */
+    public static int NumDigits(int number)
+    {
+        boolean includeNegativeSign = false;
+        return NumDigits(number, includeNegativeSign);
+    }
+    /**
+     * Returns the number of digits of a given integer
+     */
+    public static int NumDigits(int number, boolean includeNegativeSign)
+    {
+        int retVal = 0;
+        
+        if (number < 0)
+        {
+            if (includeNegativeSign)
+            {
+                ++retVal;
+            }
+            number *= -1;
+        }
+        
+        int counter = number;
+        while (counter > 0)
+        {
+            counter /= 10;
+            ++retVal;
         }
         
         return retVal;

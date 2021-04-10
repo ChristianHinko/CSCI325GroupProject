0487528c8ff2cdc71e85bef1da5b536d07bd9925
include-iostream <c4hinkle@gmail.com>
2021-04-10 18:27:33 -0300

    LAST COMMIT WAS BROKEN - accidentally added Program.java. Fix it by including new GeometryCalculator

---
+++ AdvancedCalculator3000/src/advancedcalculator3000/Calculator/GeometryCalculator.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package advancedcalculator3000.Calculator;
+
+import java.util.Scanner;
+
+
+
+/**
+ * 
+ * @author Christian Hinkle
+ */
+public class GeometryCalculator implements ICalculator
+{
+    //@Override
+    public static String GetCalculatorName()
+    {
+        return "Geometry Calculator";
+    }
+    
+    @Override
+    public void InitialUserMenu(Scanner scanner)
+    {
+        
+    }
+    
+    @Override
+    public void PrintUserMenu()
+    {
+        
+    }
+    @Override
+    public void HandleUserMenu(Scanner scanner, char userSelection)
+    {
+        
+    }
+}
